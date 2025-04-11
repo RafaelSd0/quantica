bfa526d5d3a5fa5e7731b34e3ea1fee1cc809497
Rafael <player08901@gmail.com>
2025-04-11 09:59:45 -0300

    responsividade

--- src/app/experimento/[name]/page.jsx
+++ src/app/experimento/[name]/page.jsx
@@ -17,34 +17,34 @@ export default function ExperimentPage() {
   return (
     <div className="pt-15">
       <div className="xl:px-[10%] lg:px-[5%] px-[3%] ">
-        <h1 className="w-3/5 pt-5">{experiment.title}</h1>
-        <span>{experiment.sub}</span>
-        <div className=" flex justify-between items-center py-5 flex-row-reverse">
-          <img src={experiment.image1} alt="image1" className=" max-w-[600px] rounded-2xl "/>
-          <p className="w-3/6">{experiment.text1}</p>
+        <h1 className="lg:w-3/5  pt-5 lg:text-left">{experiment.title}</h1>
+        <span >{experiment.sub}</span>
+        <div className=" flex justify-between items-center  py-5 lg:flex-row-reverse flex-col space-y-5 lg:space-y-0">
+          <img src={experiment.image1} alt="image1" className=" xl:max-w-[500px] md:max-w-[450px] lg:max-w-[350px] max-w-[350px] rounded-2xl "/>
+          <p className="lg:w-3/6 w-5/6">{experiment.text1}</p>
         </div>
-        <div className=" flex justify-between items-center py-5 ">
-          <img src={experiment.image2} alt="image2" className=" max-w-[600px] rounded-2xl " />
-          <p className="w-3/6" >{experiment.text2}</p>
+        <div className=" flex justify-between items-center py-5 lg:flex-row flex-col space-y-5 lg:space-y-0 ">
+          <img src={experiment.image2} alt="image2" className=" xl:max-w-[500px] md:max-w-[450px] lg:max-w-[350px] max-w-[350px] rounded-2xl " />
+          <p className="lg:w-3/6 w-5/6" >{experiment.text2}</p>
         </div>
       </div>
-      <div className="bg-verde-600 xl:px-[10%] lg:px-[5%] px-[3%] space-y-5 ">
+      <div className="bg-verde-600 xl:px-[10%] lg:px-[5%] px-[3%] space-y-5  ">
         <h2 className="pt-5">Faça você Mesmo</h2>
-        <p className="w-1/2">{experiment.objective}</p>
+        <p className="xl:w-1/2 lg:w-4/6 w-5/6">{experiment.objective}</p>
         <h2>Materiais Necessários</h2>
-        <ul className="w-1/2 space-y-5">
+        <ul className="xl:w-1/2 lg:w-4/6 w-5/6 space-y-5">
           { experiment.materials.map( (item, index) => (
             <li key={index}> {item} </li>
           ))}
         </ul>
         <h2>Montagem do Experimento</h2>
-        <ul className="w-1/2 space-y-5">
+        <ul className="xl:w-1/2 lg:w-4/6 w-5/6 space-y-5">
           { experiment.montage.map( (item, index) => (
             <li key={index}>{item}</li>
           ))}
         </ul>
         <h2>Procedimento</h2>
-        <ul className="w-1/2 space-y-5 pb-5">
+        <ul className="xl:w-1/2 lg:w-4/6 w-5/6 space-y-5 pb-5">
           { experiment.procedure.map( (item, index) => (
             <li key={index}>{item}</li>
           ))}
